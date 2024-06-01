dbe1c03b50a4865f92ef74d1092f7947a03a314e
Alexis <Alexis@DESKTOP-C5RDDHT>
2024-05-31 18:51:10 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo04/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejemplo04
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
---
+++ Ejemplo04/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejemplo04
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
